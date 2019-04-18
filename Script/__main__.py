17ed868e79c21ac3a032d317e737e235683a5c12
Kleber Vieira <klebermagno@gmail.com>
2019-04-18 16:52:41 -0300

    add changes

---
+++ Script/__main__.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+
+
+import matplotlib.pyplot as plt
+
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+
+print("Hello")
